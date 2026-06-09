00425242ae42dcbf19b4e126340ddcc8d640ddaf
theogicquel-weup <theo.gicquel@weuplearning.com>
2026-06-10 01:47:46 +0200

    remove img

--- src/containers/CoursesPanel/NoCoursesView/index.jsx
+++ src/containers/CoursesPanel/NoCoursesView/index.jsx
@@ -19,7 +19,6 @@ export const NoCoursesView = () => {
       id="no-courses-content-view"
       className="d-flex align-items-center justify-content-center mb-4.5"
     >
-      <Image src={emptyCourseSVG} alt={formatMessage(messages.bannerAlt)} />
       <h3 className="h1">
         {formatMessage(messages.lookingForChallengePrompt)}
       </h3>
