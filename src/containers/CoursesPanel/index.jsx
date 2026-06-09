6425162e4fcd9cbec4cb256248aae1e80bca3953
theogicquel-weup <theo.gicquel@weuplearning.com>
2026-06-10 01:54:40 +0200

    remove course filter button

--- src/containers/CoursesPanel/index.jsx
+++ src/containers/CoursesPanel/index.jsx
@@ -60,9 +60,7 @@ export const CoursesPanel = () => {
     <div className="course-list-container">
       <div className="course-list-heading-container">
         <h2 className="course-list-title">{formatMessage(messages.myCourses)}</h2>
-        <div className="course-filter-controls-container">
-          <CourseFilterControls />
-        </div>
+  
       </div>
       {hasCourses ? <CourseListSlot courseListData={courseListData} /> : <NoCoursesViewSlot />}
     </div>
